56b71007af99b00e3ad7ea119f7fcadd9175184d
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-10-29 21:41:27 -0400

    Initial commit

---
+++ othello.py
@@ -0,0 +1,172 @@
+import random
+
+
+# this class stores an othello board state
+# the state is handled as a 1d list that stores a 10x10 board.  1 and -1 are the two colors, 0 are empty squares
+class Board:
+    # make a starting board.  There are four pieces in the center
+    def __init__(self):
+        self.state = [0] * 100
+        self.state[44] = 1
+        self.state[45] = -1
+        self.state[54] = -1
+        self.state[55] = 1
+
+        # returns the score as the difference between the number of 1s and the number of -1s
+
+    def evaluate(self):
+        value = 0
+        for i in range(100):
+            if self.state[i] == 1:
+                value = value + 1
+            elif self.state[i] == -1:
+                value = value - 1
+        return value
+
+        # returns a new board that is a copy of the current board
+
+    def copy(self):
+        board = Board()
+        for i in range(100):
+            board.state[i] = self.state[i]
+        return board
+
+    # given a x,y position, returns the tile within the 1d list
+    def index(self, x, y):
+        if 0 <= x < 10 and 0 <= y < 10:
+            return self.state[x + y * 10]
+        else:
+            # out of bounds, return -2 for error
+            return -2
+
+    # given an x,y coordinate, and an id of 1 or -1, returns true if this is a valid move
+    def can_place(self, x, y, id):
+        # square is not empty? return false
+        if self.index(x, y) != 0:
+            return False
+        # these functions compute the 8 different directions
+        dirs = [
+            (lambda x: x, lambda y: y - 1),
+            (lambda x: x, lambda y: y + 1),
+            (lambda x: x - 1, lambda y: y - 1),
+            (lambda x: x - 1, lambda y: y),
+            (lambda x: x - 1, lambda y: y + 1),
+            (lambda x: x + 1, lambda y: y - 1),
+            (lambda x: x + 1, lambda y: y),
+            (lambda x: x + 1, lambda y: y + 1),
+        ]
+        # for each direction...
+        for xop, yop in dirs:
+            # move one space.  is the piece the opponent's color?
+            i, j = xop(x), yop(y)
+            if self.index(i, j) != -id:
+                # no, then we'll move on to the next direction
+                continue
+            # keep going until we hit our own piece
+            i, j = xop(i), yop(j)
+            while self.index(i, j) == -id:
+                i, j = xop(i), yop(j)
+            # if we found a piece of our own color, then this is a valid move
+            if self.index(i, j) == id:
+                return True
+                # if I can't capture in any direction, I can't place here
+        return False
+
+        # given an x,y coordinate, and an id of 1 or -1, place a tile (if valid) at x,y, and modify the state accordingly
+
+    def place(self, x, y, id):
+        # don't bother if it isn't a valid move
+        if not self.can_place(x, y, id):
+            return
+        # place your piece at x,y
+        self.state[x + y * 10] = id
+        dirs = [
+            (lambda x: x, lambda y: y - 1),
+            (lambda x: x, lambda y: y + 1),
+            (lambda x: x - 1, lambda y: y - 1),
+            (lambda x: x - 1, lambda y: y),
+            (lambda x: x - 1, lambda y: y + 1),
+            (lambda x: x + 1, lambda y: y - 1),
+            (lambda x: x + 1, lambda y: y),
+            (lambda x: x + 1, lambda y: y + 1),
+        ]
+        # go through each direction
+        for xop, yop in dirs:
+            i, j = xop(x), yop(y)
+            # move one space.  is the piece the opponent's color?
+            if self.index(i, j) != -id:
+                # no, then we can't capture in this direction.  we'll move on to the next one
+                continue
+            # keep going until we hit our own piece
+            while self.index(i, j) == -id:
+                i, j = xop(i), yop(j)
+            # if we found a piece of our own color, then this is a valid move
+            if self.index(i, j) == id:
+                k, l = xop(x), yop(y)
+                # go back and flip all the pieces to my color
+                while k != i or l != j:
+                    self.state[k + l * 10] = id
+                    k, l = xop(k), yop(l)
+
+    # returns a list of all valid x,y moves for a given id
+    def valid_moves(self, id):
+        moves = []
+        for x in range(10):
+            for y in range(10):
+                if self.can_place(x, y, id):
+                    moves = moves + [(x, y)]
+        return moves
+
+        # print out the board.  1 is X, -1 is O
+
+    def print_board(self):
+        for y in range(10):
+            line = ""
+            for x in range(10):
+                if self.index(x, y) == 1:
+                    line = line + "X"
+                elif self.index(x, y) == -1:
+                    line = line + "O"
+                else:
+                    line = line + "."
+            print(line)
+
+            # state is an end game if there are no empty places
+
+    def end(self):
+        return not 0 in self.state
+
+
+# this plays a game between two players that will play completely randomly
+def game():
+    # make the starting board
+    board = Board()
+    # start with player 1
+    turn = 1
+    while True:
+        # get the moves
+        movelist = board.valid_moves(turn)
+        # no moves, skip the turn
+        if len(movelist) == 0:
+            turn = -turn
+            continue
+        # pick a move totally at random
+        i = random.randint(0, len(movelist) - 1)
+        # make a new board
+        board = board.copy()
+        # make the move
+        board.place(movelist[i][0], movelist[i][1], turn)
+        # swap players
+        turn = -turn
+        # print
+        board.print_board()
+        # wait for user to press a key
+        input()
+        # game over? stop.
+        if board.end():
+            break
+    print("Score is", board.evaluate())
+
+
+if __name__ == "__main__":
+    game()
